e4c3e6ca7f299f35532e1866151cbc4f98233002
Yasser El Hassad <maroc2103maroc@gmail.com>
2024-07-28 17:17:45 +0100

    sql

--- task_4.sql
+++ task_4.sql
@@ -1,4 +1,4 @@
-USER alx_book_store;
-
-INSERT INTO Customers (customer_id, customer_name, email, address)
-VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
+SELECT *
+FROM INFORMATION_SCHEMA.COLUMNS
+WHERE TABLE_SCHEMA = 'alx_book_store' AND TABLE_NAME = 'books'
+ORDER BY ORDINAL_POSITION;
